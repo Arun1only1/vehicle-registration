2d796631c7a8bf65339e80ac486eaaf4b6bb8629
Arun1only1 <arun1nly1@gmail.com>
2023-12-13 17:05:48 +0545

    feat: middleware implemented

--- vehicle/vehicle.routes.js
+++ vehicle/vehicle.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
   addVehicle,
+  checkIfVehicleExists,
   deleteVehicle,
   getVehicleDetails,
   updateVehicle,
   validateVehicleData,
+  validateVehicleId,
 } from "./vehicle.service.js";
 
 const router = express.Router();
@@ -13,14 +15,32 @@ const router = express.Router();
 router.post("/add", validateVehicleData, addVehicle);
 
 // get vehicle
-router.get("/details/:id", getVehicleDetails);
+router.get("/details/:id", validateVehicleId, getVehicleDetails);
 
 // delete vehicle
-router.delete("/delete/:id", deleteVehicle);
+router.delete(
+  "/delete/:id",
+  validateVehicleId,
+  checkIfVehicleExists,
+  deleteVehicle
+);
 
 // update  vehicle
 // which vehicle is to be updated
 // which values are to be updated
-router.put("/update/:id", updateVehicle);
+
+// middlewares
+// 1.validate new values extracted from req.body
+//2. validate vehicle id for mongo id validation
+// 3.to check if vehicle with id exists or not
+// 4. update vehicle data
+
+router.put(
+  "/update/:id",
+  validateVehicleData,
+  validateVehicleId,
+  checkIfVehicleExists,
+  updateVehicle
+);
 
 export default router;
